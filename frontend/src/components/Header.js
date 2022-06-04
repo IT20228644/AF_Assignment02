23bc52e8bfc72f34c4a30dbf99c3fe58b8efac9f
RMRAmadushanka <it19147260@my.sliit.lk>
2022-06-04 23:48:59 +0530

    Frontend update added

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import "./Header.css";
+// import avatar from '../../assets/avatar.svg'
+import { useSelector } from "react-redux";
+
+const Navbar = ({ sidebarOpen, openSidebar }) => {
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userCred } = userLogin;
+    return (
+        <nav className='navbar'>
+            <div className='nav_icon' onClick={() => openSidebar()}>
+                <i className='fa fa-bars' aria-hidden='true'></i>
+            </div>
+            <p>Research Project Management Tool</p>
+            {/* <div className='navbar__left'>
+        <a href='#'>Subscribers</a>
+        <a href='#'>Video Management</a>
+        <a className='active_link' href='#'>
+          Admin
+        </a>
+      </div>
+       */}
+        </nav>
+    );
+};
+
+export default Navbar;
